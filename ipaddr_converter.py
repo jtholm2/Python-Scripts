4a055589e749c35e9e11c4ac431a0bfaa704f52b
jtholm2 <jtholmes94@gmail.com>
2020-07-27 10:09:29 -0700

    Made the ipaddr_converter.py more generic, allowing the user to input the absolute filepath of the file containing the IP addresses.

--- ipaddr_converter.py
+++ ipaddr_converter.py
@@ -1,7 +1,6 @@
-ipAddrList = ["192.168.1.1", "192.168.1.2", "192.168.1.3", "192.168.1.4"]
+
 
 def convert_ip_addr(listOfIpAddrs):
-    
     for i in range(len(listOfIpAddrs)):
         newIp = "10."
         subIp = listOfIpAddrs[i][4:]
@@ -10,5 +9,17 @@ def convert_ip_addr(listOfIpAddrs):
     return listOfIpAddrs
 
 
-print(convert_ip_addr(ipAddrList))
-        
+filePath = input("Please enter the absolute file path of the list of IP addresses you would like to convert: ")
+newFilePath = input("Please enter the absolute file path of where you would like to save the new IPs to: ")
+ipAddrs = open(filePath)
+newList = ipAddrs.readline().split(" ")
+updatedList = convert_ip_addr(newList)
+
+ipAddrs.close()
+
+new_ips = open(newFilePath, 'w')
+
+new_ips.write(" ".join(updatedList))
+
+new_ips.close()
+
